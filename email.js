d0253f8d0151c1fa0dbc19970bd6b9bdb2281d79
syadav-deqode <syadav@deqode.com>
2021-12-08 16:52:36 +0530

    Adding arrow function.

--- email.js
+++ email.js
@@ -2,10 +2,13 @@ const p = /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/;
 
 const email = 'test@test.io'
 
-function isValidEmail(email) {
-  const valid = email.match(p)
-  return valid ? true : false
-}
+// function isValidEmail(email) {
+//   const valid = email.match(p)
+//   return valid ? true : false
+// }
+
+// We can write with arrow function
+const isValidEmail = email => email.match(p) ? true : false
 
 const result = isValidEmail(email)
 
