fcbe8a1502170b50eabc9fecd380ccbb355af879
syadav-deqode <syadav@deqode.com>
2021-12-10 10:30:32 +0530

    Shorten way.

--- credit_card.js
+++ credit_card.js
@@ -1,11 +1,13 @@
 const visaRgx = /^(?:4[0-9]{12}(?:[0-9]{3})?)$/
-
-function isValidVisaCard(cardNumber) {
-  const valid = String(cardNumber).match(visaRgx)
-  return valid ? true : false
-}
-
 const visaCard = 4012888888881881
+
+// function isValidVisaCard(cardNumber) {
+//   const valid = String(cardNumber).match(visaRgx)
+//   return valid ? true : false
+// }
+
+// Es6 way
+const isValidVisaCard = cardNumber => String(cardNumber).match(visaRgx) ? true : false
 
 const isCardValid = isValidVisaCard(visaCard)
 
